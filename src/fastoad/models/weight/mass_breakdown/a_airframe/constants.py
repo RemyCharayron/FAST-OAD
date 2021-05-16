741a2900cee6ed23a8b84c38fd87e527a5c5a2ce
Christophe DAVID <christophe.david@onera.fr>
2021-05-16 18:09:36 +0200

    Now using submodels for airframe mass breakdown

---
+++ src/fastoad/models/weight/mass_breakdown/a_airframe/constants.py
@@ -0,0 +1,20 @@
+#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+SERVICE_WING_WEIGHT = "service.weight.airframe.wing"
+SERVICE_FUSELAGE_WEIGHT = "service.weight.airframe.fuselage"
+SERVICE_EMPENNAGE_WEIGHT = "service.weight.airframe.empennage"
+SERVICE_FLIGHT_CONTROLS_WEIGHT = "service.weight.airframe.flight_controls"
+SERVICE_LANDING_GEARS_WEIGHT = "service.weight.airframe.landing_gear"
+SERVICE_PYLONS_WEIGHT = "service.weight.airframe.pylons"
+SERVICE_PAINT_WEIGHT = "service.weight.airframe.paint"
