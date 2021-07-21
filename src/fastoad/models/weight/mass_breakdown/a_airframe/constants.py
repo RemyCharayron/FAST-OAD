ecd5468804c81de00d317cbcac176fa587792218
Christophe DAVID <christophe.david@onera.fr>
2021-07-21 15:27:26 +0200

    Change in submodel service naming
    [skip ci]

--- src/fastoad/models/weight/mass_breakdown/a_airframe/constants.py
+++ src/fastoad/models/weight/mass_breakdown/a_airframe/constants.py
@@ -1,5 +1,5 @@
-#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
+#  This file is part of FAST : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2020  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -11,10 +11,10 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-SERVICE_WING_WEIGHT = "service.weight.airframe.wing"
-SERVICE_FUSELAGE_WEIGHT = "service.weight.airframe.fuselage"
-SERVICE_EMPENNAGE_WEIGHT = "service.weight.airframe.empennage"
-SERVICE_FLIGHT_CONTROLS_WEIGHT = "service.weight.airframe.flight_controls"
-SERVICE_LANDING_GEARS_WEIGHT = "service.weight.airframe.landing_gear"
-SERVICE_PYLONS_WEIGHT = "service.weight.airframe.pylons"
-SERVICE_PAINT_WEIGHT = "service.weight.airframe.paint"
+SERVICE_WING_WEIGHT = "service.mass.airframe.wing"
+SERVICE_FUSELAGE_WEIGHT = "service.mass.airframe.fuselage"
+SERVICE_EMPENNAGE_WEIGHT = "service.mass.airframe.empennage"
+SERVICE_FLIGHT_CONTROLS_WEIGHT = "service.mass.airframe.flight_controls"
+SERVICE_LANDING_GEARS_WEIGHT = "service.mass.airframe.landing_gear"
+SERVICE_PYLONS_WEIGHT = "service.mass.airframe.pylons"
+SERVICE_PAINT_WEIGHT = "service.mass.airframe.paint"
